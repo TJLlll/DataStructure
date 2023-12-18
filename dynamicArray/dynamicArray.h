c5256ad93a740f5cde7c5f9b704366d0905df3ff
tan <1356026577@qq.com>
2023-12-18 18:50:20 +0800

    [万能类型动态数组]

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -1,25 +1,30 @@
 #ifndef __DYNAMIC_ARRAY_H_
 #define __DYNAMIC_ARRAY_H_
 
-typedef int ELEMENTTYPE;
+#if 1
+#define ELEMENTTYPE void*   /* 万能指针 */
+#else
+typedef void * ELEMENTTYPE;
+#endif 
+
 
 /* 避免头文件重复包含 */
 typedef struct dynamicArray
 {
-    ELEMENTTYPE *data;       /* 数组的空间 */
-    int len;        /* 数组的大小 */
-    int capacity;   /* 数组的容量 */
-}dynamicArray;
+    ELEMENTTYPE *data;  /* 数组的空间 */
+    int len;            /* 数组的大小 */
+    int capacity;       /* 数组的容量 */
+} dynamicArray;
 
 /* API: application program interface. */
 
 /* 动态数组的初始化 */
 int dynamicArrayInit(dynamicArray *pArray, int capacity);
 
-/* 动态数组插入数据(默认插到数组的末尾) */
+/* 动态数组插入数据(默认查到数组的末尾) */
 int dynamicArrayInsertData(dynamicArray *pArray, ELEMENTTYPE val);
 
-/* 动态数组插入数据，在指定位置插入 */
+/* 动态数组插入数据, 在指定位置插入 */
 int dynamicArrayAppointPosInsertData(dynamicArray *pArray, int pos, ELEMENTTYPE val);
 
 /* 动态数组修改指定位置的数据 */
@@ -32,15 +37,18 @@ int dynamicArrayDeleteData(dynamicArray *pArray);
 int dynamicArrayDeleteAppointPosData(dynamicArray *pArray, int pos);
 
 /* 动态数组删除指定的元素 */
-int dynamicArrayDeleteAppointData(dynamicArray *pArray,ELEMENTTYPE val);
+int dynamicArrayDeleteAppointData(dynamicArray *pArray, ELEMENTTYPE val);
 
 /* 动态数组的销毁 */
 int dynamicArrayDestroy(dynamicArray *pArray);
 
 /* 获取数组的大小 */
-int dynamicArrayGetSize(dynamicArray *pArray);
+int dynamicArrayGetSize(dynamicArray *pArray, int *pSize);
 
-/* 获取动态数组的容量 */
-int dynamicArrayGetCapacity(dynamicArray, int *pCapacity);
+/* 获取数组的容量 */
+int dynamicArrayGetCapacity(dynamicArray *pArray, int *pCapacity);
 
-#endif  //  __DYNAMIC_ARRAY_H_
+/* 获取指定位置的元素数据 */
+int dynamicArrayGetAppointPosVal(dynamicArray *pArray, int pos, ELEMENTTYPE *pVal);
+
+#endif // __DYNAMIC_ARRAY_H _
